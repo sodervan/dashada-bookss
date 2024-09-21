bb859f35b7e9511d8708ed513ccd1eaacc5ad7ac
sodervan <egbowonemmanuel99@gmail.com>
2024-09-21 13:23:51 +0100

    another commits

--- src/main.js
+++ src/main.js
@@ -6,10 +6,19 @@ import App from "./App.vue";
 import Body from "./components/Body.vue";
 import Countdown from "vue3-flip-countdown";
 import UPDATE from "./components/UPDATE.vue";
+import { createStore } from "vuex";
 import BookDetailsPage from "./components/BookDetailsPage.vue";
 
 
 // // Vuex Store
+const store = createStore({
+  state() {
+    return {
+      toggleMenu: false,
+      books: [],
+    };
+  },
+});
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,7 +37,7 @@ const router = createRouter({
 const app = createApp(App);
 // const { createVuetify } = Vuetify;
 // const vuetify = createVuetify();
-// app.use(store);
+app.use(store);
 app.use(router);
 app.use(Countdown);
 // app.use(vuetify);
